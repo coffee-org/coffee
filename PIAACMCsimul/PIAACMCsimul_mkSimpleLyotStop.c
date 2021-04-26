59a88383d024945374be6312816b815fa84579ee
oguyon <oliv.guyon@gmail.com>
2021-04-25 15:28:50 -1000

    remove unused var, cleanup

--- PIAACMCsimul/PIAACMCsimul_mkSimpleLyotStop.c
+++ PIAACMCsimul/PIAACMCsimul_mkSimpleLyotStop.c
@@ -1,19 +1,19 @@
 /**
  * @file    PIAACMCsimul_mkSimpleLyotStop.c
  * @brief   PIAA-type coronagraph design, run
- * 
+ *
  * Can design both APLCMC and PIAACMC coronagraphs
- *  
- * 
+ *
+ *
  * ## Change log
- * - 20180323  Guyon   minor syntax cleanup   
- * 
- * 
+ * - 20180323  Guyon   minor syntax cleanup
+ *
+ *
  * @author  O. Guyon
  *
- * 
+ *
  * @bug No known bugs.
- * 
+ *
  */
 
 
@@ -60,35 +60,38 @@ extern PIAACMCsimul_varType piaacmcsimul_var;
 
 // transmits between rin and rout
 long PIAACMCsimul_mkSimpleLyotStop(
-	const char *ID_name, 
-	float rin, 
-	float rout
-	)
+    const char *ID_name,
+    float rin,
+    float rout
+)
 {
-    long size;
-    long size2;
-    long ii, k;
-    long ID, IDr;
-    float r;
+    uint32_t size;
+    uint64_t size2;
+    imageID ID, IDr;
 
-	#ifdef PIAASIMUL_LOGFUNC0
-		PIAACMCsimul_logFunctionCall("PIAACMCsimul.fcall.log", __FUNCTION__, __LINE__, "");
-	#endif
+#ifdef PIAASIMUL_LOGFUNC0
+    PIAACMCsimul_logFunctionCall("PIAACMCsimul.fcall.log", __FUNCTION__, __LINE__,
+                                 "");
+#endif
 
 
     size = piaacmc[0].size;
-    size2 = size*size;
+    size2 = size * size;
 
-	IDr = image_ID("rcoord");
-	
+    IDr = image_ID("rcoord");
+
     ID = create_3Dimage_ID(ID_name, size, size, piaacmc[0].nblambda);
-    for(k=0; k<piaacmc[0].nblambda; k++)
-        for(ii=0; ii<size2; ii++)
+    for(long k = 0; k < piaacmc[0].nblambda; k++)
+        for(uint64_t ii = 0; ii < size2; ii++)
         {
-            if((data.image[IDr].array.F[ii]<rout)&&(data.image[IDr].array.F[ii]>rin))
-                data.image[ID].array.F[k*size2+ii] = 1.0;
+            if((data.image[IDr].array.F[ii] < rout) && (data.image[IDr].array.F[ii] > rin))
+            {
+                data.image[ID].array.F[k * size2 + ii] = 1.0;
+            }
             else
-                data.image[ID].array.F[k*size2+ii] = 0.0;
+            {
+                data.image[ID].array.F[k * size2 + ii] = 0.0;
+            }
         }
 
     return(ID);
