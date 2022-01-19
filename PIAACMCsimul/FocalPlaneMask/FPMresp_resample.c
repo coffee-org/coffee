a7daf57d651471b5415b2054a79af1b84193c4ac
Olivier Guyon <oliv.guyon@gmail.com>
2022-01-18 20:11:36 -1000

    fix include file order

--- PIAACMCsimul/FocalPlaneMask/FPMresp_resample.c
+++ PIAACMCsimul/FocalPlaneMask/FPMresp_resample.c
@@ -11,8 +11,9 @@
 #include <stdlib.h>
 
 // milk includes
-#include "COREMOD_memory/COREMOD_memory.h"
 #include "CommandLineInterface/CLIcore.h"
+
+#include "COREMOD_memory/COREMOD_memory.h"
 
 // Local variables pointers
 static char *FPMrespinimname;
